307aec5fe5c71d915257ebe205fc03fdf1815a87
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-02-24 21:58:48 +0530

    Added All Data Of Products

--- ProductReviewManagementWithLinq/Management.cs
+++ ProductReviewManagementWithLinq/Management.cs
@@ -8,26 +8,15 @@ namespace ProductReviewManagementWithLinq
 {
     public class Management
     {
-       public void ProductPrint(List<ProductReview> productReviews)
-       {
+        public void ProductPrint(List<ProductReview> productReviews)
+        {
             Console.WriteLine("Displaying all data");
-            foreach(var product in productReviews)
+            foreach (var product in productReviews)
             {
                 Console.WriteLine($"ProductId: {product.ProductId}, UserId: {product.UserId}, Rating: {product.Rating}, Review: {product.Review}, IsLike: {product.IsLike}");
             }
-       }
-       public void PrintTopThreeRecords(List<ProductReview> productReviews)
-       {
-            var result = productReviews.OrderByDescending(x => x.Rating).Take(3).ToList();
-            ProductPrint(result);
-       }
-        public void RetriveProductId(List<ProductReview> productReviews)
-        {
-            var Result = productReviews.Select(x => x.ProductId).ToList();
-            foreach (var ProductReview in Result)
-            {
-                Console.WriteLine(productReviews);
-            }
         }
+
     }
 }
+
--- ProductReviewManagementWithLinq/Program.cs
+++ ProductReviewManagementWithLinq/Program.cs
@@ -3,42 +3,41 @@ using System.Collections.Generic;
 
 namespace ProductReviewManagementWitLinq
 {
-    class Program
+    internal class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine("Dispalying ProductManganementReview");
-            List<ProductReview> productReviews = new List<ProductReview>();
+            List<ProductReview> productReviews = new List<ProductReview>()
             {
-
-                new ProductReview() { ProductId = 1, UserId = 1, Rating = 5, Review = "Good", IsLike = true };
-                new ProductReview() { ProductId = 2, UserId = 1, Rating = 3, Review = "Good", IsLike = true };
-                new ProductReview() { ProductId = 3, UserId = 1, Rating = 4, Review = "Good", IsLike = true };
-                new ProductReview() { ProductId = 4, UserId = 1, Rating = 1, Review = "Good", IsLike = true };
-                new ProductReview() { ProductId = 1, UserId = 1, Rating = 5, Review = "Good", IsLike = true };
-                new ProductReview() { ProductId = 9, UserId = 1, Rating = 2, Review = "Good", IsLike = true };
-                new ProductReview() { ProductId = 7, UserId = 1, Rating = 5, Review = "Good", IsLike = true };
-                new ProductReview() { ProductId = 8, UserId = 1, Rating = 2, Review = "Good", IsLike = true };
-                new ProductReview() { ProductId = 9, UserId = 1, Rating = 5, Review = "Good", IsLike = true };
-                new ProductReview() { ProductId = 1, UserId = 1, Rating = 5, Review = "Good", IsLike = true };
-                new ProductReview() { ProductId = 4, UserId = 1, Rating = 4, Review = "Good", IsLike = true };
-                new ProductReview() { ProductId = 12, UserId = 1, Rating = 2, Review = "Good", IsLike = true };
-                new ProductReview() { ProductId = 13, UserId = 1, Rating = 3, Review = "Good", IsLike = true };
-                new ProductReview() { ProductId = 14, UserId = 1, Rating = 4, Review = "Good", IsLike = true };
-                new ProductReview() { ProductId = 15, UserId = 1, Rating = 2, Review = "Good", IsLike = true };
-                new ProductReview() { ProductId = 1, UserId = 1, Rating = 5, Review = "Good", IsLike = true };
-                new ProductReview() { ProductId = 17, UserId = 1, Rating = 3, Review = "Good", IsLike = true };
-                new ProductReview() { ProductId = 18, UserId = 1, Rating = 2, Review = "Good", IsLike = true };
-                new ProductReview() { ProductId = 4, UserId = 1, Rating = 1, Review = "Good", IsLike = true };
-                new ProductReview() { ProductId = 9, UserId = 1, Rating = 4, Review = "Good", IsLike = true };
-
-
+                new ProductReview() { ProductId = 2, UserId = 1, Rating = 3, Review = "Good", IsLike = true },
+                new ProductReview() { ProductId = 3, UserId = 3, Rating = 4, Review = "Good", IsLike = true },
+                new ProductReview() { ProductId = 4, UserId = 3, Rating = 1, Review = "Good", IsLike = true },
+                new ProductReview() { ProductId = 1, UserId = 6, Rating = 5, Review = "Good", IsLike = true },
+                new ProductReview() { ProductId = 9, UserId = 8, Rating = 2, Review = "Good", IsLike = true },
+                new ProductReview() { ProductId = 7, UserId = 2, Rating = 5, Review = "Good", IsLike = true },
+                new ProductReview() { ProductId = 8, UserId = 3, Rating = 2, Review = "Good", IsLike = true },
+                new ProductReview() { ProductId = 9, UserId = 4, Rating = 5, Review = "Good", IsLike = true },
+                new ProductReview() { ProductId = 1, UserId = 7, Rating = 5, Review = "Good", IsLike = true },
+                new ProductReview() { ProductId = 4, UserId = 7, Rating = 4, Review = "Good", IsLike = true },
+                new ProductReview() { ProductId = 12, UserId = 1, Rating = 2, Review = "Good", IsLike = true },
+                new ProductReview() { ProductId = 13, UserId = 3, Rating = 3, Review = "Good", IsLike = true },
+                new ProductReview() { ProductId = 14, UserId = 6, Rating = 4, Review = "Good", IsLike = true },
+                new ProductReview() { ProductId = 15, UserId = 6, Rating = 2, Review = "Good", IsLike = true },
+                new ProductReview() { ProductId = 1, UserId = 8, Rating = 5, Review = "Good", IsLike = true },
+                new ProductReview() { ProductId = 17, UserId = 9, Rating = 3, Review = "Good", IsLike = true },
+                new ProductReview() { ProductId = 18, UserId = 11, Rating = 2, Review = "Good", IsLike = true },
+                new ProductReview() { ProductId = 4, UserId = 15, Rating = 1, Review = "Good", IsLike = true },
+                new ProductReview() { ProductId = 9, UserId = 5, Rating = 4, Review = "Good", IsLike = true },
             };
 
-            Management management = new Management();
-            // management.ProductPrint(productReviews);
-            // management.RetriveProductId(productReviews);
-            management.PrintTopThreeRecords(productReviews);
+            foreach (var list in productReviews)
+            {
+                Console.WriteLine("ProductId:" + list.ProductId + " " + "UserId:-" + list.UserId + " " + "Rating:-" + list.Rating + " " + "Review" + list.Review + " " + "IsLike" + list.IsLike);
+            }
+
+
+
         }
     }
 }
